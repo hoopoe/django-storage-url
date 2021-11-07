90610ddd36b9200e4bae4cf3e4fa4392e440c194
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2021-11-07 22:33:21 +0300

    try obs from 0.5.0

--- django_storage_url/backends/__init__.py
+++ django_storage_url/backends/__init__.py
@@ -6,6 +6,7 @@ SCHEMES = {
     "az": "django_storage_url.backends.az.AzureStorage",
     "file": "django_storage_url.backends.file.FileSystemStorage",
     "s3": "django_storage_url.backends.s3.S3Storage",
+    "obs": "django_storage_url.backends.obs.S3Storage",
 }
 
 
